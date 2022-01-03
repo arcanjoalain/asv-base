4d550602adb16fc86b469d6384508963aa212e97
Alain S Vieira <arcanjoalain@gmail.com>
2022-01-03 15:24:42 -0400

    update version

--- src/main/java/br/com/asv/base/client/ws/Response.java
+++ src/main/java/br/com/asv/base/client/ws/Response.java
@@ -7,16 +7,20 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class Response<
 	D,
 	O extends Serializable,
 	E extends Serializable> implements IResponse{
 
 	private static final long serialVersionUID = 1925385826590639221L;
+	@Getter
+	@Setter
 	private D data;
+	@Getter
+	@Setter
 	private O options;
+	@Getter
+	@Setter
 	private List<E> errors = new LinkedList<>();
 }
 
